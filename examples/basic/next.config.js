b285120f8b24b68f613eadb70735bb0cab9f1414
Jonas Strassel <info@jonas-strassel.de>
2023-01-04 21:57:33 +0100

    test: demonstrate failed passing of a function to
    ... nextTranslate inside the next.config.js

--- examples/basic/next.config.js
+++ examples/basic/next.config.js
@@ -1,3 +1,25 @@
+const assert = require('assert')
 const nextTranslate = require('next-translate')
 
-module.exports = nextTranslate()
+// ref.: https://nextjs.org/docs/api-reference/next.config.js/introduction
+let baseConfig = () => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    output: 'standalone',
+  }
+  return nextConfig
+}
+
+assert.equal(baseConfig().output, 'standalone')
+
+let calledFunctionConfig = nextTranslate(baseConfig())
+
+assert.equal(calledFunctionConfig.output, 'standalone')
+
+let functionConfig = nextTranslate(baseConfig)
+
+assert.equal(functionConfig.output, 'standalone')
+
+module.exports = nextTranslate({})
